6b8aa42d63a7e6fd146699c48b81d9a0d37635b1
Gabriel Lee <gabriel.lee@central1.com>
2020-02-06 00:13:45 -0800

    Implement interactive and file input functionality

--- otp.py
+++ otp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.7
+
 import sys
 import argparse
 import binascii
@@ -6,31 +8,39 @@ import random
 def main(argv):
     # Define script description and the arugment list
     parser = argparse.ArgumentParser(description='Encrypt and decrypt a One-Time Pad Cipher.')
-
     mode = parser.add_mutually_exclusive_group(required=True)
     mode.add_argument('-e', '--encrypt', help='encrypt a plaintext', action='store_true')
     mode.add_argument('-d', '--decrypt', help='decrypt a ciphertext', action='store_true')
-
-    # TODO: Implement interactive and input file functionality
     enc = parser.add_mutually_exclusive_group(required='-e' or '--encrypt' or '-d' or '--decrypt' in argv)
     enc.add_argument('-t', '--text', help='the plaintext to encrypt')
     enc.add_argument('-i', '--interactive', help='interactively provide the plaintext', action='store_true')
     enc.add_argument('-f', '--file', help='name of the input file')
-
     parser.add_argument('-k', '--key', help='the decryption key')
-
-    # TODO: Implemtn output file capability
+    # TODO: Implement output file capability
     parser.add_argument('-o', '--outputfile', help='name of the output text file')
+    args = parser.parse_args()
+
+    # Handle different input functionality
+    pt = ""
+    if args.text is not None:
+        pt = args.text
+    elif args.interactive is True:
+        pt = input("Enter plaintext: ")
+    elif args.file is not None:
+        pt = open(args.file, "r").read()
 
     # Handle encryption or decryption logic
-    args = parser.parse_args()
     if args.encrypt is True:
-        encrypt_otp(args.text)
+        encrypt_otp(pt)
     elif args.decrypt is True:
-        if args.key is None:
+        # Handle key input functionality
+        key = ""
+        if args.interactive is True:
+            key = input("Enter key: ")
+        if args.key is None and not key:
             parser.error("argument -k/--key is required")
             exit()
-        decrypt_otp(args.text, args.key)
+        decrypt_otp(pt, key)
 
 def generate_binary_key(length):
     """
@@ -55,7 +65,7 @@ def encrypt_otp(ptext):
 
     ptext - the plaintext to encrypt
     """
-    ptext_bin = bin(int(binascii.hexlify(ptext), 16))[2:]
+    ptext_bin = bin(int(binascii.hexlify(ptext.encode()), 16))[2:]
     key_bin = generate_binary_key(len(ptext_bin))
     xor_bin = xor_compare(ptext_bin,key_bin)
 
@@ -66,14 +76,14 @@ def encrypt_otp(ptext):
 
 def decrypt_otp(ctext, key):
     """
-    Perform XOR comparison using provided ciphertext and key. Display the resultin ASCII.
+    Perform XOR comparison using provided ciphertext and key. Display the result in Utf-8.
 
     ctext - the ciphertext in binary
     key - the key in binary
     """
     ctext_bin = ctext
     key_bin = key
-    res = binascii.unhexlify('%x' % int("0b"+xor_compare(ctext_bin,key_bin), 2))
+    res = binascii.unhexlify('%x' % int("0b"+xor_compare(ctext_bin,key_bin), 2)).decode()
 
     print("Ciphertext:\t{}".format(ctext_bin))
     print("Key:\t\t{}".format(key_bin))
